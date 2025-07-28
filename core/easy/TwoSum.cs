e7d13f55002f1b41e75a5c54603713bb2b3a783a
Bin Hu <elithrade@gmail.com>
2025-07-28 20:10:37 +0800

    linting and formatting

--- core/easy/TwoSum.cs
+++ core/easy/TwoSum.cs
@@ -3,35 +3,36 @@ namespace core.easy
     public class TwoSum
     {
         /// <summary>
-        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// Given an array of integers numbers and an integer target, return indices of the two numbers such that they add up to target.
         /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
         /// </summary>
-        /// <param name="nums">The array of integers.</param>
+        /// <param name="numbers">The array of integers.</param>
         /// <param name="target">The target sum.</param>
         /// <returns>An array containing the indices of the two numbers that sum up to the target.</returns>
-        public int[] Solve(int[] nums, int target)
+        public int[] Solve(int[] numbers, int target)
         {
             // A dictionary to store numbers and their indices
-            var numMap = new System.Collections.Generic.Dictionary<int, int>();
+            var numMap = new Dictionary<int, int>();
 
-            for (int i = 0; i < nums.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                int complement = target - nums[i];
+                int complement = target - numbers[i];
                 // If the complement exists in the map, we found our pair
                 if (numMap.ContainsKey(complement))
                 {
                     return new int[] { numMap[complement], i };
                 }
+
                 // Otherwise, add the current number and its index to the map
-                if (!numMap.ContainsKey(nums[i])) // Avoid adding duplicate keys if nums contains duplicates
+                if (!numMap.ContainsKey(numbers[i])) // Avoid adding duplicate keys if numbers contains duplicates
                 {
-                    numMap.Add(nums[i], i);
+                    numMap.Add(numbers[i], i);
                 }
             }
 
             // In a typical LeetCode scenario, this line would not be reached if a solution is guaranteed.
             // However, for robustness, you might throw an exception or return an empty array.
-            throw new System.ArgumentException("No two sum solution");
+            throw new ArgumentException("No two sum solution");
         }
     }
 }
--- tests/easy/TwoSumTests.cs
+++ tests/easy/TwoSumTests.cs
@@ -1,6 +1,6 @@
 using core.easy;
 
-namespace easy.tests
+namespace tests.easy
 {
     [TestFixture]
     public class TwoSumTests
@@ -9,11 +9,11 @@ namespace easy.tests
         public void TwoSum_ReturnsCorrectIndices_Example1()
         {
             var solution = new TwoSum();
-            int[] nums = { 2, 7, 11, 15 };
+            int[] numbers = { 2, 7, 11, 15 };
             int target = 9;
             int[] expected = { 0, 1 };
 
-            int[] actual = solution.Solve(nums, target);
+            int[] actual = solution.Solve(numbers, target);
 
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.Length, Is.EqualTo(expected.Length));
@@ -25,11 +25,11 @@ namespace easy.tests
         public void TwoSum_ReturnsCorrectIndices_Example2()
         {
             var solution = new TwoSum();
-            int[] nums = { 3, 2, 4 };
+            int[] numbers = { 3, 2, 4 };
             int target = 6;
             int[] expected = { 1, 2 };
 
-            int[] actual = solution.Solve(nums, target);
+            int[] actual = solution.Solve(numbers, target);
 
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.Length, Is.EqualTo(expected.Length));
@@ -41,11 +41,11 @@ namespace easy.tests
         public void TwoSum_ReturnsCorrectIndices_Example3()
         {
             var solution = new TwoSum();
-            int[] nums = { 3, 3 };
+            int[] numbers = { 3, 3 };
             int target = 6;
             int[] expected = { 0, 1 };
 
-            int[] actual = solution.Solve(nums, target);
+            int[] actual = solution.Solve(numbers, target);
 
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.Length, Is.EqualTo(expected.Length));
@@ -57,10 +57,10 @@ namespace easy.tests
         public void TwoSum_ThrowsException_NoSolution()
         {
             var solution = new TwoSum();
-            int[] nums = { 1, 2, 3 };
+            int[] numbers = { 1, 2, 3 };
             int target = 10;
 
-            Assert.Throws<System.ArgumentException>(() => solution.Solve(nums, target));
+            Assert.Throws<System.ArgumentException>(() => solution.Solve(numbers, target));
         }
     }
 }
