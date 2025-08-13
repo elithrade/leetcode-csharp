7c65f678c002ee421e3e88a68c57efe9d7ca551a
Bin Hu <elithrade@gmail.com>
2025-08-13 21:24:03 +0800

    array & hashing/medium: group anagrams (test action)

--- core/easy/GroupAnagrams.cs
+++ core/easy/GroupAnagrams.cs
@@ -1,15 +1,29 @@
-namespace core.easy
+namespace core.easy;
+
+public class GroupAnagrams
 {
-    public class GroupAnagrams
+    /// <summary>
+    /// Groups an array of strings into anagrams.
+    /// </summary>
+    /// <param name="strs">The array of strings.</param>
+    /// <returns>A list of lists, where each inner list contains anagrams.</returns>
+    public List<List<string>> Solve(string[] strs)
     {
-        /// <summary>
-        /// Groups an array of strings into anagrams.
-        /// </summary>
-        /// <param name="strs">The array of strings.</param>
-        /// <returns>A list of lists, where each inner list contains anagrams.</returns>
-        public IList<IList<string>> Solve(string[] strs)
+        var anagrams = new Dictionary<string, List<string>>();
+
+        foreach (var str in strs)
         {
-            return [];
+            // Sort the string to find its anagram group
+            var sortedStr = new string(str.OrderBy(c => c).ToArray());
+
+            if (!anagrams.ContainsKey(sortedStr))
+            {
+                anagrams[sortedStr] = new List<string>();
+            }
+
+            anagrams[sortedStr].Add(str);
         }
+
+        return anagrams.Values.ToList();
     }
 }
