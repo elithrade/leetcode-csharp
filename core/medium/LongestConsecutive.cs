a8088b10079a283335998d1a5b643124fa48a340
Bin Hu <elithrade@gmail.com>
2025-08-22 21:56:05 +0800

    made longest consecutive array more efficient by looping through the hash set

--- core/medium/LongestConsecutive.cs
+++ core/medium/LongestConsecutive.cs
@@ -11,15 +11,15 @@ public class LongestConsecutive
         // Hast set will be {2, 20, 4, 10, 3, 5}
 
         var longest = 0;
-        for (int i = 0; i < nums.Length; i++)
+        foreach (var num in numSet)
         {
             // If nums[i] - 1 is not in the set, it means nums[i] is the start of a sequence
-            if (!numSet.Contains(nums[i] - 1))
+            if (!numSet.Contains(num - 1))
             {
                 var length = 1;
                 // Look for the next consecutive numbers
                 // In the case of 2, it will try to find 2, 3, 4 until 5 which is not in the set
-                while (numSet.Contains(nums[i] + length))
+                while (numSet.Contains(num + length))
                 {
                     length++;
                 }
